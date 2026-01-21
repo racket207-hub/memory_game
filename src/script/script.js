3d7f0fd6d03a77045779d8f6612601908a51d987
racket207-hub <rocco.lore@branchina.edu.it>
2026-01-21 17:03:06 +0100

    implemented logic to create pairs of cards and shuffle them

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,8 @@
 const GAME_CONFIG = {
     difficulties: {
-        facile: { rows: 2, cols: 4 },
-        medio: { rows: 4, cols: 4 },
-        difficile: { rows: 4, cols: 6 }
+        facile: { rows: 4, cols: 4 },
+        medio: { rows: 4, cols: 6 },
+        difficile: { rows: 5, cols: 8 }
     }
 };
 
@@ -113,6 +113,7 @@ function startGame(){
         document.getElementById("start_button").style.display = "none";
         document.getElementById("display_solitaria").style.display = "block";
         startTimer(true);
+        createTable(config.rows, config.cols, config);
     });
 
 
@@ -172,7 +173,130 @@ function reset() {
     startBtn.disabled = false;
 }
 
-// function setDifficulty(difficulty){
 
+function createTable(rows, cols) {
+    const table = document.getElementById("solitaria_table");
+    table.innerHTML = "";
+
+    const carteTotali = rows * cols;
+    const numCoppie = carteTotali / 2;
+    const cards = generaCoppie(numCoppie);
+
+
+    const tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+
+    let index = 0;
+
+    for (let i = 0; i < rows; i++) {
+        const tr = document.createElement("tr");
+
+        for (let j = 0; j < cols; j++) {
+            const td = document.createElement("td");
+            td.className = "w-30 h-30 bg-white rounded-lg";
+
+            const div = document.createElement("div");
+            div.className = "w-full h-full flex items-center justify-center";
+
+            const front_card = document.createElement("div");
+            front_card.className = "w-full h-full rounded-lg";
+
+            const back_card = document.createElement("div");
+            back_card.className = "hidden bg-blue-500 w-full h-full rounded-lg";
+
+            setCardImage(front_card, cards[index]);
+            index++;
+
+            div.append(front_card, back_card);
+            td.appendChild(div);
+            tr.appendChild(td);
+        }
+
+        tbody.appendChild(tr);
+    }
+}
+
+
+// function createTable(rows, cols, config){
+//     let table = document.getElementById("solitaria_table");
+//     let k;
+//     let numeri = [];
+//     table.appendChild(document.createElement("tbody"));
+//     for(let i = 0; i < rows; i++){
+//         let tr = document.createElement("tr");
+//         for(let j = 0; j < cols; j++){
+//             let td = document.createElement("td");
+//             td.className = "w-30 h-30 bg-white rounded-lg";
+//             tr.appendChild(td);
+//             let div = td.appendChild(document.createElement("div"));
+//             div.className = "w-full h-full flex items-center justify-center text-center";
+//             div.id = `cell_${i}_${j}`;
+//             let front_card = div.appendChild(document.createElement("div"));
+//             let back_card = div.appendChild(document.createElement("div"));
+//             front_card.className = "w-full h-full rounded-lg flex items-center justify-center text-center";
+//             back_card.className = "hidden bg-blue-500 w-full h-full rounded-lg flex items-center justify-center text-center";
+//             generaImg(front_card, k, numeri);
+//         }
+//         table.querySelector("tbody").appendChild(tr);
+//     }
 // }
 
+
+function setCardImage(front_card, k) {
+    front_card.style.backgroundImage =
+        `url(../../assets/facile/emoji${k}.png)`;
+    front_card.style.backgroundSize = "60%";
+    front_card.style.backgroundRepeat = "no-repeat";
+    front_card.style.backgroundPosition = "center";
+}
+
+// function generaImg(front_card, k, numeri){
+//     k = Math.floor(Math.random() * 8) + 1;
+//     numeri.push(k);
+//     switch(usrSettings.difficulty){
+//         case "facile":
+//             const cards = generaCoppie(8);
+//             cards.forEach(card => {
+//                 front_card.style.backgroundImage = `url(../../assets/facile/emoji${card}.png)`;
+//                 front_card.style.backgroundSize = "60%";
+//                 front_card.style.backgroundRepeat = "no-repeat";
+//                 front_card.style.backgroundPosition = "center";
+//             })
+//             // while(numeri.filter(num => num === k).length > 1){
+//             //     k = Math.floor(Math.random() * 8) + 1;
+//             //     numeri.pop();
+//             //     numeri.push(k);
+//             //     if(numeri.length >= 8){
+//             //         numeri = [];
+//             //     }
+//             // }
+//             // front_card.style.backgroundImage = `url(../../assets/facile/emoji${k}.png)`;
+//             // front_card.style.backgroundSize = "60%";
+//             // front_card.style.backgroundRepeat = "no-repeat";
+//             // front_card.style.backgroundPosition = "center";
+//             break;
+//         case "medio":
+//             break;
+//         case "difficile":
+//             break;
+//     }
+// }
+
+function generaCoppie(numCoppie){
+    const arr = [];
+
+    for(let i = 1; i <= numCoppie; i++){
+        arr.push(i);
+        arr.push(i);
+    }
+
+    return shuffle(arr);
+}
+
+function shuffle(arr) {
+  	for (let i = arr.length - 1; i > 0; i--) {
+    	const j = Math.floor(Math.random() * (i + 1));
+    	[arr[i], arr[j]] = [arr[j], arr[i]];
+  	}
+  	return arr;
+}
